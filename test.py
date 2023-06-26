4442814d10b181dd3521fb8cba9924f431db5cee
Laleh Samadfam <laleh.samadfam@gmail.com>
2023-06-26 13:17:59 +0200

    minor edits

--- test.py
+++ test.py
@@ -1,83 +1,49 @@
-import cv2
 import numpy as np
-from brightest_quadrilateral import find_area, find_max_patches
-import unittest
+import cv2 as cv
 
 
-class BrightestQuadrilateralTest(unittest.TestCase):
-    # Test case : There exist a convex solution
-    def test_find_area_convex(self):
-        corners = [(0, 0), (0, 100), (100, 0), (100, 100)]
-        expected_area = 10000
-        result_area, result_order = find_area(corners)
-        self.assertEqual(result_area, expected_area)
-
-    # Test case :  Quadrilateral - non convex
-    def test_find_area_non_convex(self):
-        corners = [(0, 0), (0, 100), (100, 0), (30, 30)]
-        expected_areas = [3000, 4500]
-        result_area, result_order = find_area(corners)
-        self.assertIn(result_area, expected_areas)
-
-    # Test case : Triangle
-    def test_find_area_triangle(self):
-        corners = [(0, 10), (0, 30), (0, 80), (100, 30)]
-        expected_area = 3500
-        result_area, result_order = find_area(corners)
-        self.assertEqual(result_area, expected_area)
-
-    # Test case : Line
-    def test_find_area_line(self):
-        corners = [(0, 0), (0, 4), (0, 6), (0, 8)]
-        expected_area = 0
-        result_area, result_order = find_area(corners)
-        self.assertEqual(result_area, expected_area)
-
-    # Test case: Line - not parallel to image axes
-    def test_find_area_none_axe_line(self):
-        corners = [(0, 0), (2, 2), (4, 4), (8, 8)]
-        expected_area = 0
-        result_area, result_order = find_area(corners)
-        self.assertEqual(result_area, expected_area)
-
-    # Test case 4 separate patches
-    def test_brightest_patches_scattered(self):
-
-        image = np.zeros((300, 300), dtype=np.uint8)
-
-        # Set the 3x3 patches at specified positions
-        image[10:13, 10:13] = 255
-        image[100:103, 100:103] = 200
-        image[250:253, 250:253] = 150
-        image[50:53, 50:53] = 50
-
-        num_patches = 4
-        patch_size = [3, 3]
-
-        # Expected output
-        expected_result = [(11, 11), (101, 101), (251, 251), (51, 51)]
-
-        # Call the function
-        result = find_max_patches(image, num_patches, patch_size)
-        self.assertEqual(result, expected_result)
-
-    # Test case 2: Overlapping patches
-    def test_brightest_patches_overlapping(self):
-        image = np.zeros((300, 300), dtype=np.uint8)
-
-        # Set the 25x25 white block at position (10, 10)
-        image[10:35, 10:35] = 255
-
-        num_patches = 4
-        patch_size = [5, 5]
-
-        # Expected output
-        expected_result = [(12, 12), (12, 17), (12, 22), (12, 27)]
-
-        # Call the function
-        result = find_max_patches(image, num_patches, patch_size)
-        self.assertEqual(result, expected_result)
+def cross_product_2d(vector1, vector2):
+    """
+    takes two vectors as tuples, and returns their 2d cross product.
+    """
+    return vector1[0] * vector2[1] - vector1[1] * vector2[0]
 
 
-if __name__ == '__main__':
-    unittest.main()
+def argmax_2d(matrix):
+    """
+    returns indexes of the cell in a matrix where it has the maximum value
+    """
+    max_index = np.argmax(matrix)
+    index_2d = np.unravel_index(max_index, matrix.shape)
+    return index_2d
+
+
+def tuple_to_vector(vertices):
+    """
+    takes a list of two tuples, and returns the vector connecting the first to the second tuple.
+    """
+    a, b = vertices
+    return [b[0] - a[0], b[1] - a[1]]
+
+
+def draw(image, corners):
+    """
+    draw a polygon with the given corners on the input image
+    :param image: the input image to draw the polygon on
+    :param corners: the corners of the polygon
+    :return:
+    """
+    corners = swap_touples(corners)
+    corners = np.array(corners, dtype=np.int32)
+    corners = corners.reshape((-1, 1, 2))
+    colored_image = cv.cvtColor(image, cv.COLOR_GRAY2BGR)
+
+    cv.polylines(colored_image, [corners], isClosed=True, color=(0, 0, 255), thickness=1)
+    cv.imwrite("polygon.png", colored_image)
+
+
+def swap_touples(lst):
+    """
+    takes a list of tuples and returns a list where the items in every tuple is swapped.
+    """
+    return [(t[1], t[0]) for t in lst]
