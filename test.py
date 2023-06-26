0a12dd66ce4ab24aba643ac49d6a4867fb04157d
Laleh Samadfam <laleh.samadfam@gmail.com>
2023-06-26 13:16:57 +0200

    separate test functions

--- test.py
+++ test.py
@@ -5,39 +5,44 @@ import unittest
 
 
 class BrightestQuadrilateralTest(unittest.TestCase):
-    def test_find_area(self):
-        # Test case 1:  Quadrilateral - There exist a convex solution
+    # Test case : There exist a convex solution
+    def test_find_area_convex(self):
         corners = [(0, 0), (0, 100), (100, 0), (100, 100)]
         expected_area = 10000
         result_area, result_order = find_area(corners)
         self.assertEqual(result_area, expected_area)
 
-        # Test case 1:  Quadrilateral - non convex
+    # Test case :  Quadrilateral - non convex
+    def test_find_area_non_convex(self):
         corners = [(0, 0), (0, 100), (100, 0), (30, 30)]
         expected_areas = [3000, 4500]
         result_area, result_order = find_area(corners)
         self.assertIn(result_area, expected_areas)
 
-        # Test case 2: Triangle
+    # Test case : Triangle
+    def test_find_area_triangle(self):
         corners = [(0, 10), (0, 30), (0, 80), (100, 30)]
         expected_area = 3500
         result_area, result_order = find_area(corners)
         self.assertEqual(result_area, expected_area)
 
-        # Test case 3: Line
+    # Test case : Line
+    def test_find_area_line(self):
         corners = [(0, 0), (0, 4), (0, 6), (0, 8)]
         expected_area = 0
         result_area, result_order = find_area(corners)
         self.assertEqual(result_area, expected_area)
 
-        # Test case 3: Line - not parallel to image axes
+    # Test case: Line - not parallel to image axes
+    def test_find_area_none_axe_line(self):
         corners = [(0, 0), (2, 2), (4, 4), (8, 8)]
         expected_area = 0
         result_area, result_order = find_area(corners)
         self.assertEqual(result_area, expected_area)
 
-    def test_brightest_patches(self):
-        # Test case 1: 4 separate patches
+    # Test case 4 separate patches
+    def test_brightest_patches_scattered(self):
+
         image = np.zeros((300, 300), dtype=np.uint8)
 
         # Set the 3x3 patches at specified positions
@@ -56,7 +61,8 @@ class BrightestQuadrilateralTest(unittest.TestCase):
         result = find_max_patches(image, num_patches, patch_size)
         self.assertEqual(result, expected_result)
 
-        # Test case 2: Overlapping patches
+    # Test case 2: Overlapping patches
+    def test_brightest_patches_overlapping(self):
         image = np.zeros((300, 300), dtype=np.uint8)
 
         # Set the 25x25 white block at position (10, 10)
@@ -75,4 +81,3 @@ class BrightestQuadrilateralTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
